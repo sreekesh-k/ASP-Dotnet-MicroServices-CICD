a632ca1d49df00cde25f5aa5cdff299d3d5b06ae
Sreekesh K Prabhu <145774649+sreekesh-k@users.noreply.github.com>
2025-01-06 12:44:31 +0530

    dockerise

--- StudentsApi/Program.cs
+++ StudentsApi/Program.cs
@@ -3,21 +3,30 @@ using StudentsApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddControllers();
-builder.Services.AddDbContext<StudentDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("StudentDb")));
+builder.Services.AddDbContext<StudentDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("StudentDb")));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 app.UseForwardedHeaders();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.MapControllers();
 app.UseHttpsRedirection();
-app.Run();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<StudentDbContext>();
+    dbContext.Database.Migrate();
+}
+
+app.Run();
